6ec78995e5153950d7649f38e63acbb85e43d7e6
Aaron Holbrook <aaron@zeek.com>
2021-03-15 16:04:24 -0500

    remove unused files. add support for specific exclusions

--- src/WpSentry.php
+++ src/WpSentry.php
@@ -3,6 +3,7 @@
 namespace Zeek\WpSentry;
 
 use Sentry\ClientBuilder;
+use Sentry\Event;
 use Sentry\SentrySdk;
 use Sentry\State\Hub;
 use Sentry\State\Scope;
@@ -10,13 +11,18 @@ use Zeek\Modernity\Patterns\Singleton;
 
 class WpSentry extends Singleton {
 
-	public const SENTRY_ERROR_TYPES = 'SENTRY_ERROR_TYPES';
-
 	private const SDK_IDENTIFIER = 'sentry.php.wp-sentry-integration';
 	private const SDK_VERSION = '4.2.0';
 
 	private $client;
 	private $sentryDsn;
+
+	private $coreExclusions = [
+		'Parameter 1 to wp_default_scripts() expected to be a reference, value given',
+		'Parameter 1 to wp_default_styles() expected to be a reference, value given',
+		'Parameter 1 to wp_default_packages() expected to be a reference, value given',
+		'session_start(): Cannot start session when headers already sent',
+	];
 
 	protected function __construct() {
 		$this->initSentryClient();
@@ -25,8 +31,12 @@ class WpSentry extends Singleton {
 	private function get_dsn() : ?string {
 		$this->sentryDsn = get_env_value( 'SENTRY_DSN' );
 
-		if ( empty( $sentryDsn ) ) {
+		if ( empty( $this->sentryDsn ) ) {
 			$this->sentryDsn = get_env_value( 'SENTRY_URL' );
+		}
+
+		if ( empty( $this->sentryDsn ) ) {
+			$this->sentryDsn = E_ALL & ~E_NOTICE & ~E_WARNING;
 		}
 
 		return $this->sentryDsn;
@@ -34,14 +44,14 @@ class WpSentry extends Singleton {
 
 	private function get_default_options() : array {
 		$options = [
-			'dsn'              => $this->get_dsn(),
-			'prefixes'         => [ ABSPATH ],
-			'environment'      => get_env_value( 'ENVIRONMENT' ),
-			'send_default_pii' => defined( 'WP_SENTRY_SEND_DEFAULT_PII' ) ? WP_SENTRY_SEND_DEFAULT_PII : false,
+			'dsn'         => $this->get_dsn(),
+			'prefixes'    => [ ABSPATH ],
+			'environment' => get_env_value( 'ENVIRONMENT' ),
 		];
 
-		if ( get_env_value( self::SENTRY_ERROR_TYPES ) ) {
-			$options['error_types'] = get_env_value( self::SENTRY_ERROR_TYPES );
+		$sentryErrorLevel = get_env_value( 'SENTRY_ERROR_LEVEL' );
+		if ( ! empty( $sentryErrorLevel ) ) {
+			$options['error_types'] = $sentryErrorLevel;
 		}
 
 		$options['in_app_exclude'] = [
@@ -49,7 +59,25 @@ class WpSentry extends Singleton {
 			ABSPATH . 'wp-includes',
 		];
 
-		// @todo add paths to exclude
+		$options['before_send'] = function ( Event $event ) : ?Event {
+			$excludeEvents = array_merge( $this->coreExclusions, get_env_value( 'SENTRY_EXCLUDE_EVENTS' ) ?? []);
+
+			// Errors always get reported
+			if ( (string) $event->getLevel() === 'error' ) {
+				return $event;
+			}
+
+			// perform filtering here
+			$eventLabel = $event->getExceptions()[0]->getValue();
+
+			foreach ( $excludeEvents as $excludeEvent ) {
+				if ( $eventLabel === $excludeEvent ) {
+					return null;
+				}
+			}
+
+			return $event;
+		};
 
 		return $options;
 	}
